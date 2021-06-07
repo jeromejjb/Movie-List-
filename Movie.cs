92789c4b067716bbb0f7f82169cf5d79596a541d
jerome <jeromebrown97@gmail.com>
2021-06-07 16:03:25 -0500

    First commit for lab 10

---
+++ Movie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MovieList_Lab_10
+{
+
+    public enum Genre
+    {
+        [Description("1 - Horror")]
+        Horror = 1,
+        Comedy,
+        Romance,
+        Action,
+        Biograhy,
+        ComedyDrama,
+        Drama,
+        Thriller
+
+    }
+
+
+    class Movie
+    {
+        public string Title { get; set; }
+        public int Runtime { get; set; }
+
+        public Genre genre { get; set; }
+
+        public Movie(string title, int runtime, Genre genre)
+        {
+            Title = title;
+            Runtime = runtime;
+            this.genre = genre;
+        }
+
+        internal bool OrderBy(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieList_Lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make movie class
+            //Create Genre Enum 
+            //Allow user to search by genre
+
+            List<Movie> movies = new List<Movie>();
+
+            Movie m1 = new Movie("Goodfellas", 180, Genre.Action);
+
+            movies.Add(m1);
+
+            Movie m2 = new Movie("GodFather",178 , Genre.Action);
+            movies.Add(m2);
+
+            Movie m3 = new Movie("Good Will Hunting", 180, Genre.Romance);
+            movies.Add(m3);
+
+            Movie m4 = new Movie("Rudy", 115, Genre.Biograhy);
+            movies.Add(m4);
+
+            Movie m5 = new Movie("Dead Poet Society", 140, Genre.ComedyDrama);
+            movies.Add(m5);
+
+            Movie m6 = new Movie("The Perks of Being a Wallflower", 106, Genre.Romance);
+            movies.Add(m6);
+
+            Movie m7 = new Movie("The Social Network", 121, Genre.Drama);
+            movies.Add(m7);
+
+            Movie m8 = new Movie("Snowden", 174, Genre.Thriller);
+            movies.Add(m8);
+
+            Movie m9 = new Movie("The Conjuring: The Devil Made Me Do It", 122, Genre.Horror);
+            movies.Add(m9);
+
+            Movie m10 = new Movie("The Hangover", 100, Genre.Comedy);
+            movies.Add(m10);
+
+            Console.WriteLine("Welcome to the Movie List Applitcation!");
+
+            string userAnswer = "y";
+
+            while (userAnswer == "y")
+            {
+                Console.WriteLine("There are 10 movies in this list");
+                
+                Genre[] acceptedGenres = (Genre[])Enum.GetValues(typeof(Genre));
+
+                foreach (Genre g in acceptedGenres)
+                {
+                    Console.WriteLine(g);
+                }
+
+                Console.WriteLine("What category are you interested in? Typer a genre or enter the number beside a genre.");
+                string input = Console.ReadLine();
+
+                Genre inputGenre = (Genre)Enum.Parse(typeof(Genre), input);
+
+                foreach (Movie m in movies)
+                {
+                    if (m.genre == inputGenre)
+                    {
+                        Console.WriteLine(m.Title);
+                    }
+                }
+                Console.WriteLine("Continue (y/n)");
+                userAnswer = Console.ReadLine();
+            }
+            
+        }
+    }
+}
